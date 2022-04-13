3c9539a2b686b8c5fcb0ecdf7d7abeb93affd647
Reeceds <32424143+Reeceds@users.noreply.github.com>
2022-04-13 15:37:29 +0100

    Add score to db in progress

--- src/pages/PersonalDetails.jsx
+++ src/pages/PersonalDetails.jsx
@@ -1,13 +1,25 @@
-import { React, useState, useEffect } from 'react';
+import { React, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Form, Button, Row, Col } from 'react-bootstrap';
+import { Form, Button, Row, Col } from "react-bootstrap";
 
-import { db } from '../config/firebase-config';
-import { collection, query, where, getDocs, updateDoc, doc, serverTimestamp } from 'firebase/firestore';
+import { db } from "../config/firebase-config";
+import {
+    collection,
+    query,
+    where,
+    getDocs,
+    updateDoc,
+    doc,
+    serverTimestamp,
+} from "firebase/firestore";
 
 export default function PersonalDetails() {
-
-    const [details, setDetails] = useState({firstName: '', lastName: '', email: '', alias: ''});
+    const [details, setDetails] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        alias: "",
+    });
     const [validated, setValidated] = useState(false);
 
     let navigate = useNavigate();
@@ -18,93 +30,128 @@ export default function PersonalDetails() {
     let emailAddress = localStorage.getItem("email");
     let alias = localStorage.getItem("alias");
 
-    
     useEffect(() => {
-        setDetails({firstName: fName, lastName: lName, email: emailAddress, alias: alias});
-    },[]);
-
+        setDetails({
+            firstName: fName,
+            lastName: lName,
+            email: emailAddress,
+            alias: alias,
+        });
+    }, []);
 
     // Updates users alias in db/local storage
     const updateUserAlias = async (userEmail) => {
-        const q = query(collection(db, "users"), where("email", "==", userEmail));
+        const q = query(
+            collection(db, "users"),
+            where("email", "==", userEmail)
+        );
         const querySnapshot = await getDocs(q);
-        
+
         let userDocId;
 
         querySnapshot.forEach((doc) => {
-            userDocId = doc.id
+            userDocId = doc.id;
         });
 
         const userDocRef = doc(db, "users", userDocId);
 
         await updateDoc(userDocRef, {
             alias: details.alias,
-            timestamp: serverTimestamp()
+            timestamp: serverTimestamp(),
         });
-        
-        localStorage.setItem('alias', details.alias);
-    };
 
+        localStorage.setItem("alias", details.alias);
+    };
 
     // Checks if form is valid and updates db/navigates to requested page
     const handleSubmit = (e) => {
         e.preventDefault();
         e.stopPropagation();
-        
+
         const form = e.currentTarget;
 
         // Checks if form is valid
         if (form.checkValidity() === true) {
             // Updates users alias in db/local storage
             updateUserAlias(emailAddress);
-    
+
             // navigates to requested page
-            navigate('/');
+            navigate("/");
         }
 
         setValidated(true);
-
     };
 
-
     return (
-
         <Row>
             <Col lg={4}>
                 <Form noValidate validated={validated} onSubmit={handleSubmit}>
                     <Form.Group className="mb-3">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="text" placeholder={details.email || ''} disabled />
-                        <Form.Text className="text-muted"> We'll never share your email with anyone else.</Form.Text>
+                        <Form.Control
+                            type="text"
+                            placeholder={details.email || ""}
+                            disabled
+                        />
+                        <Form.Text className="text-muted">
+                            {" "}
+                            We'll never share your email with anyone else.
+                        </Form.Text>
                     </Form.Group>
 
                     <Form.Group className="mb-3">
                         <Form.Label>First Name</Form.Label>
-                        <Form.Control type="text" placeholder={details.firstName || ''} disabled />
+                        <Form.Control
+                            type="text"
+                            placeholder={details.firstName || ""}
+                            disabled
+                        />
                     </Form.Group>
-                    
+
                     <Form.Group className="mb-3">
                         <Form.Label>Last Name</Form.Label>
-                        <Form.Control type="text" placeholder={details.lastName || ''} disabled />
+                        <Form.Control
+                            type="text"
+                            placeholder={details.lastName || ""}
+                            disabled
+                        />
                     </Form.Group>
-                    
+
                     {/* <Form.Group className="mb-3">
                         <Form.Label>Last Name</Form.Label>
                         <Form.Control type="text" placeholder="Enter last name" value={details.lastName || ''} onChange={(e) => setDetails({...details, lastName: (e.target.value)})} />
                     </Form.Group> */}
-                    
+
                     <Form.Group className="mb-3">
                         <Form.Label>Quiz Alias</Form.Label>
-                        <Form.Control type="text" placeholder="Enter quiz alias" minLength={3} maxLength={3} required value={details.alias || ''}  onChange={(e) => setDetails({...details, alias: (e.target.value.toUpperCase())})} />
-                        <Form.Control.Feedback type="invalid">Please enter an alias.</Form.Control.Feedback>
-                        <Form.Text className="text-muted"> Your quiz alias must be three characters long.</Form.Text>
+                        <Form.Control
+                            type="text"
+                            placeholder="Enter quiz alias"
+                            minLength={3}
+                            maxLength={3}
+                            required
+                            value={details.alias || ""}
+                            onChange={(e) =>
+                                setDetails({
+                                    ...details,
+                                    alias: e.target.value.toUpperCase(),
+                                })
+                            }
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            Please enter an alias.
+                        </Form.Control.Feedback>
+                        <Form.Text className="text-muted">
+                            {" "}
+                            Your quiz alias must be three characters long.
+                        </Form.Text>
                     </Form.Group>
 
-                    <Button variant="primary" type="submit">Save</Button>
+                    <Button variant="primary" type="submit">
+                        Save
+                    </Button>
                 </Form>
             </Col>
         </Row>
-
-    )
-
+    );
 }
--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import 'font-awesome/css/font-awesome.min.css';
-import { useState, useEffect } from 'react';
+import React from "react";
+import "font-awesome/css/font-awesome.min.css";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import Button from '../components/Button';
-import AnswerButton from '../components/AnswerButton';
+import Button from "../components/Button";
+import AnswerButton from "../components/AnswerButton";
 
+import { auth, provider, db } from "../config/firebase-config";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import {
+    addDoc,
+    collection,
+    query,
+    where,
+    getDocs,
+    serverTimestamp,
+} from "firebase/firestore";
 
 export default function Quiz() {
-    
     let [data, setData] = useState([]);
     let [runApi, setRunApi] = useState(true);
 
@@ -15,77 +24,93 @@ export default function Quiz() {
     let [currentQuestion, setCurrentQuestion] = useState();
     let [correctAnswer, setCorrectAnswer] = useState();
     let [answers, setAnswers] = useState([]);
-    
+
     let [correctScore, setCorrectScore] = useState(0);
     let [incorrectScore, setIncorrectScore] = useState(0);
 
     let navigate = useNavigate();
+    const auth = getAuth();
 
     // Local storage varaibles
-    let userAuth = localStorage.getItem('isAuth');
-    let userAlias = localStorage.getItem('alias');
-
-
+    let userAuth = localStorage.getItem("isAuth");
+    let userAlias = localStorage.getItem("alias");
+    let latestScore = localStorage.getItem("lastScore");
 
     useEffect(() => {
         const getData = async () => {
             try {
-                const data = await fetch(`https://opentdb.com/api.php?amount=10`);
+                const data = await fetch(
+                    `https://opentdb.com/api.php?amount=10`
+                );
                 const res = await data.json();
                 return res;
-            } catch(err) {
-                console.log('Api call error: ', err);
+            } catch (err) {
+                console.log("Api call error: ", err);
             }
-        }
+        };
 
-        getData().then(response => {
-            setData(response.results)
-            console.log(response.results)
-        })
-    },[runApi]);
-    
+        getData().then((response) => {
+            setData(response.results);
+            console.log(response.results);
+        });
+    }, [runApi]);
 
     useEffect(() => {
         populateQuiz();
-    },[data]);
+    }, [data]);
 
-
+    // useEffect(() => {
+    //     onAuthStateChanged(auth, (user) => {
+    //         if (user) {
+    //             const uid = user.uid;
+    //         }
+    //     });
+    // }, []);
 
     // Parse html entities
-    const htmlDecode = input => {
+    const htmlDecode = (input) => {
         var doc = new DOMParser().parseFromString(input, "text/html");
         return doc.documentElement.textContent;
     };
 
-
     // Shuffles answers array
-    const shuffleAnswers = array => {
-        let currentIndex = array.length,  randomIndex;
-      
+    const shuffleAnswers = (array) => {
+        let currentIndex = array.length,
+            randomIndex;
+
         // While there remain elements to shuffle...
         while (currentIndex !== 0) {
-      
-          // Pick a remaining element...
-          randomIndex = Math.floor(Math.random() * currentIndex);
-          currentIndex--;
-      
-          // And swap it with the current element.
-          [array[currentIndex], array[randomIndex]] = [
-            array[randomIndex], array[currentIndex]];
-        };
-      
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            // And swap it with the current element.
+            [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex],
+                array[currentIndex],
+            ];
+        }
+
         return array;
     };
 
     // Navigate to the login page
     const goToLogin = () => {
-        navigate('/login');
-    }
+        navigate("/login");
+    };
 
     const saveScore = () => {
-        console.log('save score')
-    }
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                const uid = user.uid;
 
+                addDoc(collection(db, "users", uid, "scores"), {
+                    date: serverTimestamp(),
+                    score: latestScore,
+                });
+            }
+        });
+    };
 
     // Updates the quesiton/answers hooks with the quiz data
     const populateQuiz = () => {
@@ -95,16 +120,16 @@ export default function Quiz() {
 
         data.forEach((item, i) => {
             // If the item index is = to current question number, decode and push question/answers into variables
-            if(i === queNum) {
+            if (i === queNum) {
                 question = htmlDecode(item.question);
                 correctAns = htmlDecode(item.correct_answer);
                 answers.push(correctAns);
 
-                item.incorrect_answers.forEach(incorrectAns => {
+                item.incorrect_answers.forEach((incorrectAns) => {
                     incorrectAns = htmlDecode(incorrectAns);
                     answers.push(incorrectAns);
                 });
-            };
+            }
         });
 
         // Shuffle answers array
@@ -116,7 +141,6 @@ export default function Quiz() {
         setAnswers(answers);
     };
 
-
     // Runs the API to refresh the set of the quesitons and resets question number/score
     const newQuiz = () => {
         setRunApi(runApi ? false : true);
@@ -125,118 +149,149 @@ export default function Quiz() {
         setIncorrectScore(0);
 
         // Hide end quiz section
-        document.querySelector('.js-end-quiz-section').classList.add("hide");
+        document.querySelector(".js-end-quiz-section").classList.add("hide");
         // Unhide quiz section
-        document.querySelector('.js-quiz-section').classList.remove("hide");
+        document.querySelector(".js-quiz-section").classList.remove("hide");
     };
-    
-    
+
     // Unhides the quiz if user has entered an alias
     const startQuiz = () => {
         // If no alias, redirect to details page
-        if(userAuth === 'true' && (userAlias === null || userAlias === undefined || userAlias === '')){
-            navigate('/personalDetails');
+        if (
+            userAuth === "true" &&
+            (userAlias === null || userAlias === undefined || userAlias === "")
+        ) {
+            navigate("/personalDetails");
         }
-        
-        // Unhide quiz section
-        document.querySelector('.js-quiz-section').classList.remove("hide");
-        // Hide start quiz button
-        document.querySelector('.js-start-btn').classList.add("hide");
-        // Hide end quiz score section
-        document.querySelector('.js-end-quiz-section').classList.add("hide");
-    };
 
+        // Unhide quiz section
+        document.querySelector(".js-quiz-section").classList.remove("hide");
+        // Hide start quiz button
+        document.querySelector(".js-start-btn").classList.add("hide");
+        // Hide end quiz score section
+        document.querySelector(".js-end-quiz-section").classList.add("hide");
+    };
 
     const endQuiz = () => {
         // Hides quiz section
-        document.querySelector('.js-quiz-section').classList.add("hide");
+        document.querySelector(".js-quiz-section").classList.add("hide");
         // Hides end quiz button
-        document.querySelector('.js-end-quiz-btn').classList.add("hide");
+        document.querySelector(".js-end-quiz-btn").classList.add("hide");
         // Show end quiz score section
-        document.querySelector('.js-end-quiz-section').classList.remove("hide");
+        document.querySelector(".js-end-quiz-section").classList.remove("hide");
 
         // Add last score to local storage
-        localStorage.setItem('lastScore', correctScore);
-    }
-
+        localStorage.setItem("lastScore", correctScore);
+    };
 
     // Adds 1 to quesiton number and calls function to pupulate question/answers
     const getNextQue = () => {
-        setQueNum(queNum += 1);
+        setQueNum((queNum += 1));
         populateQuiz();
 
         // Hide next question button
-        document.querySelector('.js-next-btn').classList.add("hide");
+        document.querySelector(".js-next-btn").classList.add("hide");
     };
-
 
     // Checks what asnwer was selected
     const checkAnswer = (e, selected) => {
-
-        if(selected === correctAnswer){
-            setCorrectScore(correctScore += 1);
+        if (selected === correctAnswer) {
+            setCorrectScore((correctScore += 1));
         } else {
-            setIncorrectScore(incorrectScore += 1);
-        };
+            setIncorrectScore((incorrectScore += 1));
+        }
 
-        document.querySelectorAll('.js-answer-btn').forEach(button => {
+        document.querySelectorAll(".js-answer-btn").forEach((button) => {
             button.disabled = true;
 
-            if(button.innerText !== correctAnswer){
-                button.classList.add('wrong-answer');
+            if (button.innerText !== correctAnswer) {
+                button.classList.add("wrong-answer");
             } else {
-                button.classList.add('correct-answer');
-            };
+                button.classList.add("correct-answer");
+            }
         });
 
         // Add higlight colour to selected answer
-        e.target.classList.add('selected-answer');
+        e.target.classList.add("selected-answer");
 
-        if(queNum !== 9){
+        if (queNum !== 9) {
             // Unhide next question button
-            document.querySelector('.js-next-btn').classList.remove("hide");
+            document.querySelector(".js-next-btn").classList.remove("hide");
         } else {
             // Unhide end quiz button
-            document.querySelector('.js-end-quiz-btn').classList.remove("hide");
+            document.querySelector(".js-end-quiz-btn").classList.remove("hide");
         }
-
     };
-
-
-
 
     return (
         <div className="quiz-page">
             <h1>Quiz</h1>
 
             {/* Quiz section */}
-            <div className='quiz-section js-quiz-section hide'>
+            <div className="quiz-section js-quiz-section hide">
                 <h2>Question: {queNum + 1}</h2>
-                <h2>Correct: {correctScore}  Incorrect: {incorrectScore}</h2>
+                <h2>
+                    Correct: {correctScore} Incorrect: {incorrectScore}
+                </h2>
                 <h3>Q: {currentQuestion}</h3>
 
-                <div className='answers-section'>
-                    {answers.map(answer => {
-                        return <AnswerButton className={'answer-btn js-answer-btn'} key={answer} answer={answer} checkAnswer={(e) => checkAnswer(e, answer)} />
+                <div className="answers-section">
+                    {answers.map((answer) => {
+                        return (
+                            <AnswerButton
+                                className={"answer-btn js-answer-btn"}
+                                key={answer}
+                                answer={answer}
+                                checkAnswer={(e) => checkAnswer(e, answer)}
+                            />
+                        );
                     })}
                 </div>
-                <Button className={'js-next-btn hide'} text={'Next'} click={() => getNextQue()} />
+                <Button
+                    className={"js-next-btn hide"}
+                    text={"Next"}
+                    click={() => getNextQue()}
+                />
             </div>
-            
+
             {/* After quiz options section */}
-            <div className='end-quiz-section js-end-quiz-section hide'>
-                <h2>You scored: {correctScore}/{queNum + 1}</h2>
-                <Button className={'js-new-quiz-btn'} text={'New quiz'} click={() => newQuiz()} />
+            <div className="end-quiz-section js-end-quiz-section hide">
+                <h2>
+                    You scored: {correctScore}/{queNum + 1}
+                </h2>
+                <Button
+                    className={"js-new-quiz-btn"}
+                    text={"New quiz"}
+                    click={() => newQuiz()}
+                />
 
-                {userAuth === 'true' ? <Button text={'Save score'} click={() => saveScore()} /> : ``}
+                {userAuth === "true" ? (
+                    <Button text={"Save score"} click={() => saveScore()} />
+                ) : (
+                    ``
+                )}
             </div>
 
-            <Button className={'js-start-btn'} text={'Start quiz'} click={() => startQuiz()} />
+            <Button
+                className={"js-start-btn"}
+                text={"Start quiz"}
+                click={() => startQuiz()}
+            />
 
-            {userAuth === null ? <Button text={'Log in to save your score'} click={() => goToLogin()} /> : ``}
-            
-            <Button className={'js-end-quiz-btn hide'} text={'End quiz'} click={() => endQuiz()} />
+            {userAuth === null ? (
+                <Button
+                    text={"Log in to save your score"}
+                    click={() => goToLogin()}
+                />
+            ) : (
+                ``
+            )}
+
+            <Button
+                className={"js-end-quiz-btn hide"}
+                text={"End quiz"}
+                click={() => endQuiz()}
+            />
         </div>
     );
-    
 }
