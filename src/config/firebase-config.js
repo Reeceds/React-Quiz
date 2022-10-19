19b6c99a8de2ed8e1718de037b9f0195c24029b2
Reeceds <32424143+Reeceds@users.noreply.github.com>
2022-10-19 00:31:34 -0400

    Add environment variables

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -10,13 +10,13 @@ import { GoogleAuthProvider, getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAfdKc9Ki_YZS-N2mnUmTFEK-Ge0_j_qHo",
-  authDomain: "quiz-b31e2.firebaseapp.com",
-  projectId: "quiz-b31e2",
-  storageBucket: "quiz-b31e2.appspot.com",
-  messagingSenderId: "1008361483573",
-  appId: "1:1008361483573:web:3dfaec3532ab7f3ceeb07c",
-  measurementId: "G-3KVLS9W4DY"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENTID,
 };
 
 // Initialize Firebase
